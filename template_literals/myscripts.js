0de6aa844f6eaf2a4e24bdbb06ee15dfefbcce24
akihiko47 <akihiko4701a@gmail.com>
2023-10-16 00:04:29 +0300

    Added literals

---
+++ template_literals/myscripts.js
@@ -0,0 +1,8 @@
+let tags = ["eggs", "bread", "milk", "chicken", "oil", "cucumbers", "bananas", "tomatos"];
+
+let cnt = 0;
+
+function add_element() {
+    document.getElementById("demo").innerHTML += `<li>you need to buy ${tags[cnt%tags.length]}</li>`;
+    cnt += 1;
+}
